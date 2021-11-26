d328a3ab568a56d8fd8ae90c6540ea1cecfef02d
Patrick <zhupx2003@126.com>
2021-11-26 14:41:39 +0800

    hello

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(0 ..< 5) { item in
+                Text("Hello, world!")
+                    
+            }
+        }
     }
 }
 
