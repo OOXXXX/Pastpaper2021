d35c6a3c31577070f70258345d74015753d38d54
Patrick <zhupx2003@126.com>
2021-11-26 14:33:02 +0800

    Initial Commit

---
+++ Shared/Pastpaper2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Pastpaper2021App.swift
+//  Shared
+//
+//  Created by ZhuPinxi on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Pastpaper2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
