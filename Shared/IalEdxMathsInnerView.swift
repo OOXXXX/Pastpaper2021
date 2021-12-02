6908509e33b8362b058f6b6181a57bce53a07f1b
Patrick <zhupx2003@126.com>
2021-12-03 00:02:59 +0800

    Add WebView progress bar and constraints

--- Shared/IalEdxMathsInnerView.swift
+++ Shared/IalEdxMathsInnerView.swift
@@ -30,23 +30,88 @@ struct IalEdxMathsInnerView: View {
 
 struct IalEdxMathsInnerWebView: View {
     var body: some View {
-        WebView(url: URL(string: "https://cdn.savemyexams.co.uk/wp-content/uploads/2020/12/WMA11_01_que_20190109.pdf")!)
+        Webview(url: "https://cdn.savemyexams.co.uk/wp-content/uploads/2020/12/WMA11_01_que_20190109.pdf")
             .edgesIgnoringSafeArea(.all)
     }
         
 }
 
-struct WebView: UIViewRepresentable {
- 
-    var url: URL
- 
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+var activityIndicator: UIActivityIndicatorView!
+
+struct Webview: UIViewControllerRepresentable {
+    let url: String
+    
+
+    func makeUIViewController(context: Context) -> WebviewController {
+        let webviewController = WebviewController()
+        
+
+        
+        let request = URLRequest(url: URL(string: self.url)!, cachePolicy: .returnCacheDataElseLoad)
+        webviewController.webview.load(request)
+
+        return webviewController
     }
- 
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
+
+    func updateUIViewController(_ webviewController: WebviewController, context: Context) {
+        let request = URLRequest(url: URL(string: self.url)!, cachePolicy: .returnCacheDataElseLoad)
+        webviewController.webview.load(request)
+        
+    }
+}
+
+class WebviewController: UIViewController {
+    lazy var webview: WKWebView = WKWebView()
+    lazy var progressbar: UIProgressView = UIProgressView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.webview.frame = self.view.frame
+        self.webview.translatesAutoresizingMaskIntoConstraints = false
+        self.webview.allowsBackForwardNavigationGestures = true
+        self.view.addSubview(self.webview)
+
+        self.view.addSubview(self.progressbar)
+        self.progressbar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraints([
+            self.progressbar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.progressbar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.progressbar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            
+            self.webview.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.webview.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.webview.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.webview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            
+        ])
+        
+//        NSLayoutConstraint.activate([
+//            progressbar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+//        ])
+
+        self.progressbar.progress = 0.1
+        webview.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+    }
+
+    // MARK: - Web view progress
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        switch keyPath {
+        case "estimatedProgress":
+            if self.webview.estimatedProgress >= 1.0 {
+                UIView.animate(withDuration: 0.3, animations: { () in
+                    self.progressbar.alpha = 0.0
+                }, completion: { finished in
+                    self.progressbar.setProgress(0.0, animated: false)
+                })
+            } else {
+                self.progressbar.isHidden = false
+                self.progressbar.alpha = 1.0
+                progressbar.setProgress(Float(self.webview.estimatedProgress), animated: true)
+            }
+        default:
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
     }
 }
 
