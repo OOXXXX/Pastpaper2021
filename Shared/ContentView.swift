1b9b163c61edec37608e163551ce9291a66b0d7c
Patrick <zhupx2003@126.com>
2021-12-04 01:24:52 +0800

    Remove some trash codes

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -270,48 +270,10 @@ struct ContentView: View {
             .navigationBarTitle("Pastpaper", displayMode: .large)
             //.navigationBarHidden(false)
             .searchable(text: $searchText)
-            
-            
-        }
-        
-        
-}
-    var searchResults1: [String] {
-        if searchText.isEmpty {
-            return qualifications
-        } else {
-            return qualifications.filter {
-                $0.localizedCaseInsensitiveContains(searchText)}
-            
-        }
-    }
-
-    var searchResults2: [String] {
-        if searchText.isEmpty {
-            return bureaus
-        } else {
-            return bureaus.filter { $0.localizedCaseInsensitiveContains(searchText)}
-            
+                        
         }
     }
     
-    var searchResults3: [String] {
-        if searchText.isEmpty {
-            return extraTests
-        } else {
-            return extraTests.filter { $0.localizedCaseInsensitiveContains(searchText.lowercased()) }
-            
-        }
-    }
-    
-    var searchResults4: [String] {
-        if searchText.isEmpty {
-            return websites
-        } else {
-            return websites.filter { $0.localizedCaseInsensitiveContains(searchText.lowercased()) }
-            
-        }
-    }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
