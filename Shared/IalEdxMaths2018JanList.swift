ad04cd7de393735f03def79b195c6d38da1d7c30
Patrick <zhupx2003@126.com>
2021-12-23 18:44:22 +0800

    Added ialEdxMaths2018Jan paper

---
+++ Shared/IalEdxMaths2018JanList.swift
@@ -0,0 +1,200 @@
+//
+//  IalEdxMaths2018JanList.swift
+//  Pastpaper2021
+//
+//  Created by ZhuPinxi on 2021/12/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct IalEdxMaths2018JanView: View {
+    @State private var searchText = ""
+    @State private var showNew1 = false
+    @State private var showNew2 = false
+    @State var selected = 1
+    var body: some View {
+        VStack{
+            Picker(selection: $selected, label: Text("")){
+                Text("Question Paper").tag(1)
+                Text("Mark Scheme").tag(2)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal, 10)
+            .padding(.vertical, 6.5)
+            
+            if selected == 1{
+                IalEdxMaths2018JanList1()
+            }
+            if selected == 2{
+                IalEdxMaths2018JanList2()
+            }
+        }
+        
+        .navigationBarTitle("18 Spring", displayMode: .inline)
+        .listStyle(.plain)
+        
+    }
+    func placeOrder() { }
+    func adjustOrder() { }
+}
+
+struct IalEdxMaths2018JanList1: View {
+    var  body: some View {
+        List {
+            ForEach(IalMaths2018JanData1)  { ialMaths2018Jan1 in
+                NavigationLink(destination: IalEdxMaths2018JanWebView1(ialMaths2018Jan1: ialMaths2018Jan1)) {
+                    Text(ialMaths2018Jan1.name)
+                }
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2018JanList2: View {
+    var  body: some View {
+        List {
+            ForEach(IalMaths2018JanData2)  { ialMaths2018Jan2 in
+                NavigationLink(destination: IalEdxMaths2018JanWebView2(ialMaths2018Jan2: ialMaths2018Jan2)) {
+                    Text(ialMaths2018Jan2.name)
+                }
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2018JanWebView1: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @State private var isPresented = false
+    @State private var isActivityPopoverPresented = false
+    @State private var isActivitySheetPresented = false
+    var ialMaths2018Jan1: IalMaths2018Jan1
+    
+    var body: some View {
+        Webview(url: ialMaths2018Jan1.url)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(ialMaths2018Jan1.name, displayMode: .inline)
+            .navigationBarItems(trailing: shareButton)
+            .sheet(isPresented: $isActivitySheetPresented, content: activityView)
+        
+    }
+    private var shareButton: some View {
+        Button(action: {
+            switch (self.horizontalSizeClass, self.verticalSizeClass) {
+            case (.regular, .regular):
+                self.isActivityPopoverPresented.toggle()
+            default:
+                self.isActivitySheetPresented.toggle()
+                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                impactLight.impactOccurred()
+            }
+        }, label: {
+            Image(systemName: "square.and.arrow.up")
+                .font(.system(size: 17))
+                .frame(width: 30, height: 30)
+                .popover(isPresented: $isActivityPopoverPresented, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                    activityView()
+                }
+        })
+    }
+    
+    private func activityView() -> some View {
+        let url = URL(string: ialMaths2018Jan1.url)!
+        let filename = url.pathComponents.last!
+        let fileManager = FileManager.default
+        let itemURL = fileManager.temporaryDirectory.appendingPathComponent(filename)
+        let data: Data
+        if fileManager.fileExists(atPath: itemURL.path) {
+            data = try! Data(contentsOf: itemURL)
+        } else {
+            data = try! Data(contentsOf: url)
+            fileManager.createFile(atPath: itemURL.path, contents: data, attributes: nil)
+        }
+        let activityView = ActivityView(activityItems: [itemURL], applicationActivities: nil)
+        return Group {
+            if self.horizontalSizeClass == .regular && self.verticalSizeClass == .regular {
+                activityView.frame(width: 333, height: 480)
+            } else {
+                activityView
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2018JanWebView2: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @State private var isPresented = false
+    @State private var isActivityPopoverPresented = false
+    @State private var isActivitySheetPresented = false
+    var ialMaths2018Jan2: IalMaths2018Jan2
+    
+    var body: some View {
+        Webview(url: ialMaths2018Jan2.url)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(ialMaths2018Jan2.name, displayMode: .inline)
+            .navigationBarItems(trailing: shareButton)
+            .sheet(isPresented: $isActivitySheetPresented, content: activityView)
+        
+    }
+    private var shareButton: some View {
+        Button(action: {
+            switch (self.horizontalSizeClass, self.verticalSizeClass) {
+            case (.regular, .regular):
+                self.isActivityPopoverPresented.toggle()
+            default:
+                self.isActivitySheetPresented.toggle()
+                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                impactLight.impactOccurred()
+            }
+        }, label: {
+            Image(systemName: "square.and.arrow.up")
+                .font(.system(size: 17))
+                .frame(width: 30, height: 30)
+                .popover(isPresented: $isActivityPopoverPresented, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                    activityView()
+                }
+        })
+    }
+    
+    private func activityView() -> some View {
+        let url = URL(string: ialMaths2018Jan2.url)!
+        let filename = url.pathComponents.last!
+        let fileManager = FileManager.default
+        let itemURL = fileManager.temporaryDirectory.appendingPathComponent(filename)
+        let data: Data
+        if fileManager.fileExists(atPath: itemURL.path) {
+            data = try! Data(contentsOf: itemURL)
+        } else {
+            data = try! Data(contentsOf: url)
+            fileManager.createFile(atPath: itemURL.path, contents: data, attributes: nil)
+        }
+        let activityView = ActivityView(activityItems: [itemURL], applicationActivities: nil)
+        return Group {
+            if self.horizontalSizeClass == .regular && self.verticalSizeClass == .regular {
+                activityView.frame(width: 333, height: 480)
+            } else {
+                activityView
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct IalMaths2018Jan1: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var url: String
+}
+
+struct IalMaths2018Jan2: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var url: String
+}
+
+let IalMaths2018JanData1: [IalMaths2018Jan1] = load("IalMaths2018Jan1.json")
+let IalMaths2018JanData2: [IalMaths2018Jan2] = load("IalMaths2018Jan2.json")
+
--- Shared/IalEdxMathsViewNew.swift
+++ Shared/IalEdxMathsViewNew.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct IalEdxMathsViewTest: View {
     let impactMed = UIImpactFeedbackGenerator(style: .medium)
-        
+    
     @State private var showNew1 = false
     @State private var showNew2 = false
     @State var selected = 7
@@ -99,9 +99,9 @@ struct IalEdxMathsViewTest: View {
                                 }
                             }) {
                                 Image(systemName: "list.bullet.circle")
+                            }
                         }
-                        }
-                       
+                        
                     }
                 }
         }
@@ -190,13 +190,103 @@ struct IalEdxMathsViewTest: View {
                                 }
                             }) {
                                 Image(systemName: "list.bullet.circle")
+                            }
                         }
-                        }
-                       
+                        
                     }
                 }
         }
- 
+        if selected == 10 {
+            IalEdxMaths2018JanView()
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction){
+                        
+                        HStack {
+                            Menu() {
+                                NavigationLink(destination: EmptyView()) {
+                                    Label("Show data booklet", systemImage: "list.bullet.rectangle.portrait")
+                                }
+                                NavigationLink(destination: EmptyView()) {
+                                    Label("Show syllabus", systemImage: "character.book.closed")
+                                }
+                                NavigationLink(destination: EmptyView()) {
+                                    Label("Show course resources", systemImage: "bookmark")
+                                }
+                                Section(header: Text("Secondary actions")) {
+                                    NavigationLink(destination: EmptyView()) {
+                                        Label("Paper request", systemImage: "arrowshape.turn.up.right")
+                                    }
+                                }
+                            } label: {
+                                Image(systemName: "ellipsis.circle")
+                            }
+                            
+                            Menu(content: {
+                                Menu("2021") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(1)
+                                        Text("Summer").tag(2)
+                                        Text("Winter").tag(3)
+                                    }
+                                }
+                                Menu("2020") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(4)
+                                        Text("Summer").tag(5)
+                                        Text("Winter").tag(6)
+                                    }
+                                }
+                                Menu("2019") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(7)
+                                        Text("Summer").tag(8)
+                                        Text("Winter").tag(9)
+                                    }
+                                }
+                                Menu("2018") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(10)
+                                        Text("Summer").tag(11)
+                                        Text("Winter").tag(12)
+                                    }
+                                }
+                                Menu("2017") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(13)
+                                        Text("Summer").tag(14)
+                                        Text("Winter").tag(15)
+                                    }
+                                }
+                                Menu("2016") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(16)
+                                        Text("Summer").tag(17)
+                                        Text("Winter").tag(18)
+                                    }
+                                }
+                                Menu("2015") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(19)
+                                        Text("Summer").tag(20)
+                                        Text("Winter").tag(21)
+                                    }
+                                }
+                                Menu("2014") {
+                                    Picker(selection: $selected, label: Text("")){
+                                        Text("Spring").tag(22)
+                                        Text("Summer").tag(23)
+                                        Text("Winter").tag(24)
+                                    }
+                                }
+                            }) {
+                                Image(systemName: "list.bullet.circle")
+                            }
+                        }
+                        
+                    }
+                }
+        }
+        
     }
 }
 
