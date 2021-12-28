eae9ad71ebf74dbc31dbdc3f34796452e36767cd
Patrick <zhupx2003@126.com>
2021-12-29 01:00:22 +0800

    Added ialedxMaths2017Oct paper

---
+++ Shared/IalEdxMaths2017OctList.swift
@@ -0,0 +1,279 @@
+//
+//  IalEdxMaths2017OctList.swift
+//  Pastpaper2021
+//
+//  Created by ZhuPinxi on 2021/12/29.
+//
+
+import SwiftUI
+import WebKit
+
+struct IalEdxMaths2017OctView: View {
+    
+    @State var selected = 1
+    var body: some View {
+        VStack{
+            Picker(selection: $selected, label: Text("")){
+                Text("Question Paper").tag(1)
+                Text("Mark Scheme").tag(2)
+                Text("Examiner Report").tag(3)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal, 10)
+            .padding(.vertical, 6.5)
+            
+            if selected == 1{
+                IalEdxMaths2017OctList1()
+            }
+            if selected == 2{
+                IalEdxMaths2017OctList2()
+            }
+            if selected == 3{
+                IalEdxMaths2017OctList3()
+            }
+        }
+        .navigationBarTitle("17 Winter", displayMode: .inline)
+        .listStyle(.plain)
+    }
+}
+
+struct IalEdxMaths2017OctList1: View {
+    var  body: some View {
+        List {
+            ForEach(IalMaths2017OctData1)  { ialMaths2017Oct1 in
+                NavigationLink(destination: IalEdxMaths2017OctWebView1(ialMaths2017Oct1: ialMaths2017Oct1)) {
+                    Text(ialMaths2017Oct1.name)
+                }
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2017OctList2: View {
+    var  body: some View {
+        List {
+            ForEach(IalMaths2017OctData2)  { ialMaths2017Oct2 in
+                NavigationLink(destination: IalEdxMaths2017OctWebView2(ialMaths2017Oct2: ialMaths2017Oct2)) {
+                    Text(ialMaths2017Oct2.name)
+                }
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2017OctList3: View {
+    var  body: some View {
+        List {
+            ForEach(IalMaths2017OctData3)  { ialMaths2017Oct3 in
+                NavigationLink(destination: IalEdxMaths2017OctWebView3(ialMaths2017Oct3: ialMaths2017Oct3)) {
+                    Text(ialMaths2017Oct3.name)
+                }
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2017OctWebView1: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @State private var isPresented = false
+    @State private var isActivityPopoverPresented = false
+    @State private var isActivitySheetPresented = false
+    var ialMaths2017Oct1: IalMaths2017Oct1
+    
+    var body: some View {
+        Webview(url: ialMaths2017Oct1.url)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(ialMaths2017Oct1.name, displayMode: .inline)
+            .navigationBarItems(trailing: shareButton)
+            .sheet(isPresented: $isActivitySheetPresented, content: activityView)
+        
+    }
+    private var shareButton: some View {
+        Button(action: {
+            switch (self.horizontalSizeClass, self.verticalSizeClass) {
+            case (.regular, .regular):
+                self.isActivityPopoverPresented.toggle()
+            default:
+                self.isActivitySheetPresented.toggle()
+                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                impactLight.impactOccurred()
+            }
+        }, label: {
+            Image(systemName: "square.and.arrow.up")
+                .font(.system(size: 17))
+                .frame(width: 30, height: 30)
+                .popover(isPresented: $isActivityPopoverPresented, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                    activityView()
+                }
+        })
+    }
+    
+    private func activityView() -> some View {
+        let url = URL(string: ialMaths2017Oct1.url)!
+        let filename = url.pathComponents.last!
+        let fileManager = FileManager.default
+        let itemURL = fileManager.temporaryDirectory.appendingPathComponent(filename)
+        let data: Data
+        if fileManager.fileExists(atPath: itemURL.path) {
+            data = try! Data(contentsOf: itemURL)
+        } else {
+            data = try! Data(contentsOf: url)
+            fileManager.createFile(atPath: itemURL.path, contents: data, attributes: nil)
+        }
+        let activityView = ActivityView(activityItems: [itemURL], applicationActivities: nil)
+        return Group {
+            if self.horizontalSizeClass == .regular && self.verticalSizeClass == .regular {
+                activityView.frame(width: 333, height: 480)
+            } else {
+                activityView
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2017OctWebView2: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @State private var isPresented = false
+    @State private var isActivityPopoverPresented = false
+    @State private var isActivitySheetPresented = false
+    var ialMaths2017Oct2: IalMaths2017Oct2
+    
+    var body: some View {
+        Webview(url: ialMaths2017Oct2.url)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(ialMaths2017Oct2.name, displayMode: .inline)
+            .navigationBarItems(trailing: shareButton)
+            .sheet(isPresented: $isActivitySheetPresented, content: activityView)
+        
+    }
+    private var shareButton: some View {
+        Button(action: {
+            switch (self.horizontalSizeClass, self.verticalSizeClass) {
+            case (.regular, .regular):
+                self.isActivityPopoverPresented.toggle()
+            default:
+                self.isActivitySheetPresented.toggle()
+                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                impactLight.impactOccurred()
+            }
+        }, label: {
+            Image(systemName: "square.and.arrow.up")
+                .font(.system(size: 17))
+                .frame(width: 30, height: 30)
+                .popover(isPresented: $isActivityPopoverPresented, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                    activityView()
+                }
+        })
+    }
+    
+    private func activityView() -> some View {
+        let url = URL(string: ialMaths2017Oct2.url)!
+        let filename = url.pathComponents.last!
+        let fileManager = FileManager.default
+        let itemURL = fileManager.temporaryDirectory.appendingPathComponent(filename)
+        let data: Data
+        if fileManager.fileExists(atPath: itemURL.path) {
+            data = try! Data(contentsOf: itemURL)
+        } else {
+            data = try! Data(contentsOf: url)
+            fileManager.createFile(atPath: itemURL.path, contents: data, attributes: nil)
+        }
+        let activityView = ActivityView(activityItems: [itemURL], applicationActivities: nil)
+        return Group {
+            if self.horizontalSizeClass == .regular && self.verticalSizeClass == .regular {
+                activityView.frame(width: 333, height: 480)
+            } else {
+                activityView
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct IalEdxMaths2017OctWebView3: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @State private var isPresented = false
+    @State private var isActivityPopoverPresented = false
+    @State private var isActivitySheetPresented = false
+    var ialMaths2017Oct3: IalMaths2017Oct3
+    
+    var body: some View {
+        Webview(url: ialMaths2017Oct3.url)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(ialMaths2017Oct3.name, displayMode: .inline)
+            .navigationBarItems(trailing: shareButton)
+            .sheet(isPresented: $isActivitySheetPresented, content: activityView)
+        
+    }
+    private var shareButton: some View {
+        Button(action: {
+            switch (self.horizontalSizeClass, self.verticalSizeClass) {
+            case (.regular, .regular):
+                self.isActivityPopoverPresented.toggle()
+            default:
+                self.isActivitySheetPresented.toggle()
+                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                impactLight.impactOccurred()
+            }
+        }, label: {
+            Image(systemName: "square.and.arrow.up")
+                .font(.system(size: 17))
+                .frame(width: 30, height: 30)
+                .popover(isPresented: $isActivityPopoverPresented, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                    activityView()
+                }
+        })
+    }
+    
+    private func activityView() -> some View {
+        let url = URL(string: ialMaths2017Oct3.url)!
+        let filename = url.pathComponents.last!
+        let fileManager = FileManager.default
+        let itemURL = fileManager.temporaryDirectory.appendingPathComponent(filename)
+        let data: Data
+        if fileManager.fileExists(atPath: itemURL.path) {
+            data = try! Data(contentsOf: itemURL)
+        } else {
+            data = try! Data(contentsOf: url)
+            fileManager.createFile(atPath: itemURL.path, contents: data, attributes: nil)
+        }
+        let activityView = ActivityView(activityItems: [itemURL], applicationActivities: nil)
+        return Group {
+            if self.horizontalSizeClass == .regular && self.verticalSizeClass == .regular {
+                activityView.frame(width: 333, height: 480)
+            } else {
+                activityView
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct IalMaths2017Oct1: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var url: String
+}
+
+struct IalMaths2017Oct2: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var url: String
+}
+
+struct IalMaths2017Oct3: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var url: String
+}
+
+let IalMaths2017OctData1: [IalMaths2017Oct1] = load("IalMaths2017Oct1.json")
+let IalMaths2017OctData2: [IalMaths2017Oct2] = load("IalMaths2017Oct2.json")
+let IalMaths2017OctData3: [IalMaths2017Oct3] = load("IalMaths2017Oct3.json")
+
+
+
