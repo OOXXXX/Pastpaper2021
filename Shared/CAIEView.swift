e5d8fb73ecccc6c4ca64e8a77c572af8c6182873
Patrick <zhupx2003@126.com>
2022-01-16 00:37:49 +0800

    Test

--- Shared/CAIEView.swift
+++ Shared/CAIEView.swift
@@ -15,21 +15,12 @@ struct CAIEView: View {
             Section(header: Text("Cambridge Upper Secondary")) {
                 NavigationLink(destination: CaieIGCSEView()) {
                     HStack {
-//                        Image(systemName: "i.square")
-//                             .font(Font.system(.title))
-//                             .foregroundColor(.blue)
-                        Text("International GCSE")
+                       Text("International GCSE")
                     }
-                    //.offset(x: -8)
                 }
                 NavigationLink(destination: CaieOLView()) {
-                    HStack {
-//                        Image(systemName: "o.square")
-//                             .font(Font.system(.title))
-//                             .foregroundColor(.purple)
-                        Text("O Level")
+                       Text("O Level")
                     }
-                    //.offset(x: -8)
                 }
                 
             }
